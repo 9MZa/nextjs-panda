512bbef743326096a9dbe828a94d2d49ddeff708
Bandit Silachai <mbandit001@yahoo.co.th>
2023-08-05 14:51:09 +0700

    ✨ feature(app): add dark-light

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,18 @@
+"use client";
+
 import { css } from "../../styled-system/css";
+import { useState } from "react";
 import { Container, Center } from "../../styled-system/jsx";
 
 export default function Home() {
+  const [toggleColor, setToggleColor] = useState(false);
+
   return (
     <div
       className={css({
-        bgColor: "yellow.50",
+        bg: toggleColor ? "yellow.50" : "slate.950",
+        _motionReduce: { transition: "none" },
+        _motionSafe: { transition: "all 0.2s" },
       })}
     >
       <Container>
@@ -26,9 +33,10 @@ export default function Home() {
               _motionSafe: { transition: "all 0.3s" },
               _hover: {
                 animation: "wiggle 200ms linear infinite",
-                cursor: "not-allowed",
+                cursor: "pointer",
               },
             })}
+            onClick={() => setToggleColor((prev) => !prev)}
           >
             Hello 🐼!
           </button>
